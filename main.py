9c1efa370ea4cf0bff3a1ff0fd81e319e61a9053
Matvej Melikhov <145163462+matvej-melikhov@users.noreply.github.com>
2023-12-15 21:09:19 +0300

    Stable version

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*
+
+from app import application
+from app import migrate
+
+from app.routes import *
+from app.models import *
+
+from app import admin
+from flask_admin.contrib.sqla import ModelView
+
+from app import db
+
+class UserView(ModelView):
+    page_size = 100
+    can_export = True
+    column_display_pk = True
+    column_searchable_list = ['name', 'email']
+    column_filters = ['name', 'email']
+    form_columns = ["id", "name", "course", "grade", "email", "reason"]
+
+admin.add_view(UserView(User, db.session))
+
+
+if __name__ == "__main__":
+    application.run(debug=True)

9531ad1cd052577072c06540174ee70731234647
Matvej Melikhov <matvej.melikhov@yandex.ru>
2024-02-12 11:40:10 +0300

    configure via environment variables

--- app/config.py
+++ app/config.py
@@ -1,13 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config():
-    SECRET_KEY = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///base.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///base.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
 
-    #MAIL
-    MAIL_SERVER = "mail.hosting.reg.ru"
-    MAIL_PORT = 465
-    MAIL_USERNAME = "info@sokt-profcom.ru"
-    MAIL_PASSWORD = "sokt-profcom"
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    # MAIL
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "True") == "True"
+    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL", "False") == "True"
